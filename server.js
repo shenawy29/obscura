2990b583d9601a6281d17667fe430549de0daddb
shenawy <alshenawy10203022@gmail.com>
2024-04-28 03:22:37 +0300

    update dockerfile

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
-import { createServer } from "node:http";
-
-import next from "next";
-
-import { Server } from "socket.io";
+const { createServer } = require("node:http");
+const next = require("next");
+const { Server } = require("socket.io");
 
 const dev = process.env.NODE_ENV !== "production";
 const hostname = "localhost";
