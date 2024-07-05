4f71447b9464519a4aa6eaa5ed4ec0cfe655a451
Mohamed Elshenawy <alshenawy10203022@gmail.com>
2024-07-06 02:25:31 +0300

    update robots.txt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,25 @@
 import ChannelInput from "@/components/ChannelInput";
 
 export default function Home() {
-    return (
-        <main>
-            <div className="container px-[15%] mt-[10%] flex items-center flex-col gap-5 text-center">
-                <p className="leading-7 [&:not(:first-child)]:mt-6">
-                    welcome to obscura, a simple, lean, mean, hack.chat-like
-                    webapp. As you can see, my design is very creative. Nothing
-                    you send is saved; everything is stored in-memory, which
-                    essentially means that they&apos;re gone when you exit
-                    (sad), and I trust my hosting provider to give me a valid
-                    TLS certificate for communication to be encrypted, not that
-                    that&apos;s really special nowadays.
-                </p>
+  return (
+    <main>
+      <div className="container px-[15%] mt-[10%] flex items-center flex-col gap-5 text-center">
+        <p className="leading-7 [&:not(:first-child)]:mt-6">
+          welcome to obscura, a simple, lean, mean, hack.chat-like webapp.
+          Nothing you send is saved; everything is stored in-memory, which
+          essentially means that they&apos;re gone when you exit.
+        </p>
 
-                <p className="leading-7 [&:not(:first-child)]:mt-6">
-                    to communicate you enter a channel, a channel is basically:
-                    <br />
-                    this website&apos;s URL/channel
-                </p>
+        <p className="leading-7 [&:not(:first-child)]:mt-6">
+          to communicate you enter a channel, a channel is basically:
+          <br />
+          this website&apos;s URL/channel
+        </p>
 
-                <p className="leading-7 [&:not(:first-child)]:mt-6">
-                    below is the obligatory input helper
-                </p>
+        <p className="leading-7 [&:not(:first-child)]:mt-6">have fun!</p>
 
-                <ChannelInput />
-            </div>
-        </main>
-    );
+        <ChannelInput />
+      </div>
+    </main>
+  );
 }
